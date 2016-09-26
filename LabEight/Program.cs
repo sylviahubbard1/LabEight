8e6e1e11cc5cf4f8da78c10eb4382ee2d8ccfff8
sylviahubbard1 <sylviahubbard@gmail.com>
2016-09-26 09:36:32 -0400

    LabEight-BattingStats

--- LabEight/Program.cs
+++ LabEight/Program.cs
@@ -4,12 +4,90 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LabEight
+namespace LabEight //this is the method class you would call when you want to use it elsewhere
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-        }
+            Console.WriteLine("Welcome to Batting Average Calculator!");
+            Console.WriteLine();
+            //string contYes ="";
+            //do
+            //{
+            Console.WriteLine("Enter number of times at bat:\n");
+            int numTimesAtBat = int.Parse(Console.ReadLine());
+
+
+            int[] battingSheet = new int[numTimesAtBat];
+
+            Console.WriteLine("0=out, 1=single, 2=double, 3=triple, 4=homerun");
+            Console.WriteLine();
+            //beginning of result loop here
+            for (int i = 0; i < battingSheet.Length; i++)
+            {
+
+                Console.Write($"Result for at-bat:{i} ");
+                string answer = Console.ReadLine(); int resultBatNum = int.Parse(answer);
+                //we need to hold the value of answer in the array (initializing)
+                battingSheet[i] = resultBatNum;
+                
+            //need to put a check number for answer
+        }//end of result loop
+            Console.WriteLine("\nBatting Average:" + CalculateBattingAverage(battingSheet));
+            Console.WriteLine("\nSlugging Average: " + SluggingPercentage(battingSheet));
+            //do the loop here.
+        // double battingAvg = new CalculateBattingAverage(int[] battingSheet);
+        // double slugPercent = new SluggingPercentage(int[] battingSheet) 
     }
+
+    //the method is going to go inside the class
+
+    public static double CalculateBattingAverage(int[] battingSheet)
+
+
+    {
+        Console.WriteLine("Enter number of times at bat:\n");
+        int numTimesAtBat = int.Parse(Console.ReadLine());
+        //CONNECT THE CALCULATION TO THE OUTPUT
+
+        //method to calculate the batting average
+        double totalNumAtBats = battingSheet.Length;
+        double numerator = 0;
+        for (int i = 0; i < battingSheet.Length; i++)
+        {
+            int totalScored = battingSheet[i];
+            if (totalScored >= 1)
+            {
+                numerator = numerator + 1;
+            }
+        }
+        return numerator / totalNumAtBats;
+
+    }
+    public static double SluggingPercentage(int[] battingSheet)
+    {
+        double totalNumAtBats = battingSheet.Length;
+        double numerator = 0;
+        for (int i = 0; i < battingSheet.Length; i++)
+        {
+            int totalScored = battingSheet[1];
+            numerator = numerator + battingSheet[i];
+
+        }
+        return numerator / totalNumAtBats;
+
+    } //Console.WriteLine ("Do you want to continue?y/n \n");
+      //Console.WriteLine();
+      //}while (
 }
+}
+
+
+
+
+
+
+
+
+
---
+++ LabEightBattingTest/AverageTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LabEight;
+
+namespace LabEightBattingTest
+{
+
+    [TestClass]
+    public class AverageTest
+    {
+        [TestMethod]
+        public void TestBattingPercentageIs0whenNoRuns()
+        {
+            int[] battingSheet = new int[5];
+            //creating a new int array and naming it batting sheet which will equal new constructor and how many
+            battingSheet[0] = 0;
+            battingSheet[1] = 0;
+            battingSheet[2] = 0;
+            battingSheet[3] = 0;
+            battingSheet[4] = 0;
+            //put examples in the five places
+            //array of integers  that equals our batting sheet.
+            //input will be array output will be double average
+
+            //now we want to call a method - using Calculate for batting average
+            double average = Program.CalculateBattingAverage(battingSheet);
+            //want to call the method by going program dot calculatebattingaverage and pass it in the parenthesis
+            //thru batting sheet and make it return something "equal soemthing "(double average)
+
+            //now you must assert something
+            Assert.AreEqual(0, average); //areEqual is a method that takes two numbers (two objects) 
+            //first number is what is expected and the number that comes out in the test
+        }
+        [TestMethod]
+        public void TestBattingPercentageIs60()
+        {
+            int[] battingSheet = new int[5];
+
+            battingSheet[0] = 0;
+            battingSheet[1] = 0;
+            battingSheet[2] = 2;
+            battingSheet[3] = 4;
+            battingSheet[4] = 1;
+
+            double average = Program.CalculateBattingAverage(battingSheet);
+
+            Assert.AreEqual(0.6, average);
+        }
+    }
+}
